3d647cfcdf68af8010b401f4f5e4808ed65fa44e
pavandavanam <65418000+pavandavanam@users.noreply.github.com>
2020-05-16 03:08:29 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,8 @@
+n=int(input("enter first number n="))           #sum of five numbers
+m=int(input("enter secound number m="))
+l=int(input("enter first number l="))
+q=int(input("enter secound number q="))
+s=int(input("enter first number s="))
+p=n+m+l+q+s                         # adding a numbers 
+
+print(p)
